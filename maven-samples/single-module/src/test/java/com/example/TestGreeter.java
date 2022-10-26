306f129575bc0e91e233baa1d3cf10230deaa65c
Shey Gildas <sheygildas@gmail.com>
2022-10-26 11:34:46 +0100

    add Hello World to testgreeter

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,13 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you ";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you for making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
